d8e317878768d241b1e6544199b8f4d1550ae66b
prokudin <prokudin@glosav.ru>
2018-11-23 20:14:50 +0300

    initial idea commit

---
+++ src/main/java/ru/glosav/lad/test/springcloudtester/SpringCloudTesterApplication.java
@@ -0,0 +1,12 @@
+package ru.glosav.lad.test.springcloudtester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudTesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudTesterApplication.class, args);
+    }
+}
